7e5512461f8a4fb4cc6a73e115a86da97e7fe86a
Bert Wesarg <bert.wesarg@googlemail.com>
2016-08-19 09:33:14 +0200

    Add setup.py.
    Signed-off-by: Bert Wesarg <bert.wesarg@googlemail.com>

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = 'openssh-mux-mon',
+    version = '1.0',
+    author = 'Bert Wesarg',
+    author_email = 'bertwesarg@users.noreply.github.com',
+    description = 'A AppIndicator to monitor OpenSSH connections',
+    license = '3-clause BSD',
+    url = 'https://github.com/bertwesarg/openssh-mux-mon',
+    scripts = ['muxmon.py', 'pysshmux.py'],
+    install_requires = [
+        'pygtk>=2.24',
+        'appindicator>=12.10.1',
+        'pynotify>=0.1.1',
+        'pyinotify>=0.9.5'
+    ]
+)
